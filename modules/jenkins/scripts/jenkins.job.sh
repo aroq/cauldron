150630e7c1e2c83994b13915d79146b16afbc321
Alexander Tolstikov <tolstikov@gmail.com>
2018-10-13 19:06:45 +0300

    Update debug info

--- modules/jenkins/scripts/jenkins.job.sh
+++ modules/jenkins/scripts/jenkins.job.sh
@@ -2,16 +2,17 @@
 
 POD=$(kubectl get pod -n "${SERVICES_ZEBRA_NAMESPACE}" -l component="${SERVICES_ZEBRA_NAMESPACE}-jenkins-master" -o jsonpath='{.items[0].metadata.name}')
 
-echo "$POD"
+echo "POD: $POD"
 
+echo "kubectl cp ${SERVICES_ZEBRA_NAMESPACE}/${POD}:/var/jenkins_home/zebra/user_token jenkins_user_token"
 kubectl cp "${SERVICES_ZEBRA_NAMESPACE}/${POD}:/var/jenkins_home/zebra/user_token" jenkins_user_token
 
-JENKINS_AUTH_TOKEN=${JENKINS_AUTH_USER}:$(cat jenkins_user_token)
+JENKINS_AUTH_TOKEN="${JENKINS_AUTH_USER}:$(cat jenkins_user_token)"
 export JENKINS_AUTH_TOKEN
 
 JENKINS_HOST=${JENKINS_HOST:-$(kubectl -n "${SERVICES_ZEBRA_NAMESPACE}" get service "zebra-cd-${SERVICES_ZEBRA_ENVIRONMENT}-jenkins" -o jsonpath="{.spec.clusterIP}")}
 
-echo "$JENKINS_HOST"
+echo "JENKINS_HOST: $JENKINS_HOST"
 
-jenkins.job.py
+eval "${CAULDRON_PATH}/modules/jenkins/jenkins.job.py"
 
