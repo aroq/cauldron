72cd435558d4539c7bb76a1a7b76426a4dfa59be
Alexander Tolstikov <tolstikov@gmail.com>
2018-10-13 18:57:45 +0300

    Update cauldron

--- modules/jenkins/scripts/jenkins.job.sh
+++ modules/jenkins/scripts/jenkins.job.sh
@@ -2,12 +2,16 @@
 
 POD=$(kubectl get pod -n "${SERVICES_ZEBRA_NAMESPACE}" -l component="${SERVICES_ZEBRA_NAMESPACE}-jenkins-master" -o jsonpath='{.items[0].metadata.name}')
 
+echo "$POD"
+
 kubectl cp "${SERVICES_ZEBRA_NAMESPACE}/${POD}:/var/jenkins_home/zebra/user_token" jenkins_user_token
 
 JENKINS_AUTH_TOKEN=${JENKINS_AUTH_USER}:$(cat jenkins_user_token)
 export JENKINS_AUTH_TOKEN
 
 JENKINS_HOST=${JENKINS_HOST:-$(kubectl -n "${SERVICES_ZEBRA_NAMESPACE}" get service "zebra-cd-${SERVICES_ZEBRA_ENVIRONMENT}-jenkins" -o jsonpath="{.spec.clusterIP}")}
+
+echo "$JENKINS_HOST"
 
 jenkins.job.py
 
