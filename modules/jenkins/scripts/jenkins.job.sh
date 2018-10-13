e7638b1975affc5a6b759bae7169631f15841f40
Alexander Tolstikov <tolstikov@gmail.com>
2018-10-13 19:33:37 +0300

    Update debug

--- modules/jenkins/scripts/jenkins.job.sh
+++ modules/jenkins/scripts/jenkins.job.sh
@@ -4,11 +4,9 @@ POD=$(kubectl get pod -n "${SERVICES_ZEBRA_NAMESPACE}" -l component="${SERVICES_
 
 echo "POD: $POD"
 
-gcloud auth list
+# kubectl cp ${SERVICES_ZEBRA_NAMESPACE}/${POD}:/var/jenkins_home/zebra/user_token jenkins_user_token
 
-kubectl cp ${SERVICES_ZEBRA_NAMESPACE}/${POD}:/var/jenkins_home/zebra/user_token jenkins_user_token
-
-JENKINS_AUTH_TOKEN="${JENKINS_AUTH_USER}:$(cat jenkins_user_token)"
+JENKINS_AUTH_TOKEN="${JENKINS_AUTH_USER}:$(kubectl -n ${SERVICES_ZEBRA_NAMESPACE} exec ${POD} cat /var/jenkins_home/zebra/user_token)"
 export JENKINS_AUTH_TOKEN
 
 JENKINS_HOST=${JENKINS_HOST:-$(kubectl -n "${SERVICES_ZEBRA_NAMESPACE}" get service "zebra-cd-${SERVICES_ZEBRA_ENVIRONMENT}-jenkins" -o jsonpath="{.spec.clusterIP}")}
