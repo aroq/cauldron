b0a06050b57488fe4c0faf5273156d577f9f30cb
Alexander Tolstikov <tolstikov@gmail.com>
2018-10-15 19:44:05 +0300

    Refactor jenkins test script

--- modules/jenkins/scripts/jenkins.job.sh
+++ modules/jenkins/scripts/jenkins.job.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 POD=$(kubectl get pod -n "${SERVICES_ZEBRA_NAMESPACE}" -l component="${SERVICES_ZEBRA_NAMESPACE}-jenkins-master" -o jsonpath='{.items[0].metadata.name}')
-
 echo "POD: $POD"
+
+JENKINS_HOST=${JENKINS_HOST:-$(kubectl -n "${SERVICES_ZEBRA_NAMESPACE}" get service "zebra-cd-${SERVICES_ZEBRA_ENVIRONMENT}-jenkins" -o jsonpath="{.spec.clusterIP}")}
+echo "JENKINS_HOST: $JENKINS_HOST"
+export JENKINS_HOST
+
+JENKINS_PORT="8080"
+export JENKINS_PORT
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${JENKINS_HOST}:${JENKINS_PORT}/login)" != "200" ]]; do sleep 5; done
+
+sleep 15
 
 JENKINS_AUTH_TOKEN="${JENKINS_AUTH_USER}:$(kubectl -n ${SERVICES_ZEBRA_NAMESPACE} exec ${POD} cat /var/jenkins_home/zebra/user_token)"
 export JENKINS_AUTH_TOKEN
-
-JENKINS_HOST=${JENKINS_HOST:-$(kubectl -n "${SERVICES_ZEBRA_NAMESPACE}" get service "zebra-cd-${SERVICES_ZEBRA_ENVIRONMENT}-jenkins" -o jsonpath="{.spec.clusterIP}")}
-
-export JENKINS_HOST
-
-echo "JENKINS_HOST: $JENKINS_HOST"
 
 eval "${CAULDRON_PATH}/modules/jenkins/scripts/jenkins.job.py"
 
