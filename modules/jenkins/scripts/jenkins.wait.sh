34ede415e8edf7e72dbeb96673116827be025f2e
Alexander Tolstikov <tolstikov@gmail.com>
2019-01-11 11:42:18 +0300

    Add jenkins wait make rule

--- modules/jenkins/scripts/jenkins.wait.sh
+++ modules/jenkins/scripts/jenkins.wait.sh
@@ -8,12 +8,3 @@ JENKINS_PORT=${JENKINS_PORT:-8080}
 export JENKINS_PORT
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${JENKINS_HOST}:${JENKINS_PORT}/login)" != "200" ]]; do sleep 5; done
-
-POD=$(kubectl get pod -n "${SERVICES_ZEBRA_NAMESPACE}" -l component="${SERVICES_ZEBRA_NAMESPACE}-jenkins-master" -o jsonpath='{.items[0].metadata.name}')
-echo "POD: $POD"
-
-JENKINS_AUTH_TOKEN="${JENKINS_AUTH_USER}:$(kubectl -n ${SERVICES_ZEBRA_NAMESPACE} exec ${POD} cat /var/jenkins_home/zebra/user_token)"
-export JENKINS_AUTH_TOKEN
-
-eval "${CAULDRON_PATH}/modules/jenkins/scripts/jenkins.job.py"
-
